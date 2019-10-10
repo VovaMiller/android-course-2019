99932db9ca5bdacd21c9d344372a8efbc5dbdfb1
Vova <Miller.VV@phystech.edu>
2019-10-10 20:34:16 +0300

    Empty Project

---
+++ app/src/main/java/com/vovamiller_97/pioneer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vovamiller_97.pioneer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
